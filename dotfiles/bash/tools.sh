9e40f4ce6b1637e1f8a5a129c32fca67a0092549
Pablo Gonzalez <pbglezc@gmail.com>
2012-09-08 14:21:10 -0500

    Fix

--- dotfiles/bash/tools.sh
+++ dotfiles/bash/tools.sh
@@ -4,10 +4,20 @@ function load_keys(){
 }
 
 function setup_dev_node(){
+  # Check arguments are present
+  if [ ${#1} -le 0 ]
+  then
+    echo 'You need to specify the name of the lxc container'
+    return 1
+  fi
+
   # Check lxc-container is started
-  if[[ $(sudo lxc-info -n $1 | grep state | grep RUNNING | wc -l) -ge 1 ]] then
-    echo "is running"
-  else
-    echo "is not..."
+  if [ $(sudo lxc-info -n $1 | grep state | grep RUNNING | wc -l) -le 0 ]
+  then
+    echo "Container $1 is not running."
+    echo "To start it run:"
+    echo ""
+    echo "  \`lxc-start $1\`"
+    return 1
   fi
 }
